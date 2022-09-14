ba012ec74beaae89810c24b67ec77bf35a8ef93e
Bryan <bryan.cduran@alumnos.upm.es>
2022-09-14 18:16:00 +0200

    Added module with example component #3

---
+++ src/bryanprojects/bryanprojects.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './bryanprojects.component.html',
+  styles: [],
+})
+export class BryanprojectsComponent {
+  constructor() {}
+}
